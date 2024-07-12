7acc261f6b56ef25210860b650e83b3fcefa2c17
TWTom041 <tom0986820022@gmail.com>
2024-07-12 09:11:25 +0800

    fix numpy version

--- setup_picongpu.sh
+++ setup_picongpu.sh
@@ -74,7 +74,7 @@ spack load openpmd-api@0.15.2 %gcc \
     ^hdf5@1.14.3 \
     ^openmpi@4.1.5 +atomics +cuda cuda_arch=70 \
     ^python@3.11.6 \
-    ^py-numpy@1.26.2
+    ^py-numpy@1.26.1
 spack load boost@1.83.0 %gcc
 
 # PIConGPU output dependencies ################################################
